505e9518ce9f588cfbe7df73a62ad5e473119481
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-15 14:11:59 -0600

    Updated method names

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -6,18 +6,18 @@ class Anagram
   end
 
   def match(potential_matches)
-    filter(potential_matches).find_all do |potential_match|
-      clean(potential_match) == clean(input)
+    filter_duplicates(potential_matches).find_all do |potential_match|
+      sort_letters(potential_match) == sort_letters(input)
     end
   end
 
-private
+  private
 
-  def clean(input)
+  def sort_letters(input)
     input.downcase.split("").sort.join
   end
 
-  def filter(list_of_words)
+  def filter_duplicates(list_of_words)
     list_of_words.reject do |word|
       word.downcase == input.downcase
     end
