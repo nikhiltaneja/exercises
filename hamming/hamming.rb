5939a5b3ee47a11db91a12913369be677d2ae27f
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-25 16:30:32 -0600

    Reduced complexity

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,25 +1,19 @@
 class Hamming
 
   def self.compute(strand_one, strand_two)
+    paired_strands(strand_one, strand_two).count do |nucleotide_one, nucleotide_two|
+      mutation?(nucleotide_one, nucleotide_two)
+    end
+  end
+
+  def self.paired_strands(strand_one, strand_two)
     strand_one_nucleotides = strand_one.chars
     strand_two_nucleotides = strand_two.chars
 
-    paired_strands = strand_one_nucleotides.zip(strand_two_nucleotides)
-
-    mutations = paired_strands.find_all do |pair|
-      mutation?(pair)
-    end
-
-    mutations.length
+    strand_one_nucleotides.zip(strand_two_nucleotides)
   end
 
-  def self.mutation?(pair)
-    if pair.first.nil? || pair.last.nil?
-      return false
-    elsif pair.first != pair.last
-      true
-    else
-      false
-    end
+  def self.mutation?(nucleotide_one, nucleotide_two)
+    !(nucleotide_one.nil? || nucleotide_two.nil?) && (nucleotide_one != nucleotide_two)
   end
 end
